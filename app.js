d59eee8a015eecb17b01e3319341caf003c4364a
JhonkFl <jhonk1516f@gmail.com>
2025-07-09 14:43:49 -0600

    Formulario activo

--- app.js
+++ app.js
@@ -19,12 +19,31 @@ function seleccionar(){
 
 function gmail(){
     document.getElementById("gmail").addEventListener("click",function(){
-        const correo = "correo@example.com";
+        const correo = "mariohernandez4018h@gmail.com";
         const asunto = "Hola";
         const cuerpo = "Escribe tu mensaje aquí";
         const enlace = `mailto:${correo}?subject=${encodeURIComponent(asunto)}&body=${encodeURIComponent(cuerpo)}`;
 
         window.location.href = enlace;
     });
-
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+    const formulario = document.getElementById("contactoMsg");
+
+    formulario.addEventListener("submit",function(event) {
+        event.preventDefault();
+
+        const nombre = document.getElementById("nombre").value;
+        const puesto = document.getElementById("puesto").value;
+        const mensaje = document.getElementById("mensaje").value;
+
+        const telefono = "5217461082972"
+        const texto = `Hola, soy ${nombre}. Y soy ${puesto} : ${mensaje}`;
+        alert("Gracias por tu mensaje "+nombre+" Nos pondremos en contacto!!");
+        const enlace = `https://wa.me/${telefono}?text=${encodeURIComponent(texto)}`;
+
+        window.open(enlace, "_blank");
+        formulario.reset();
+    })
+});
